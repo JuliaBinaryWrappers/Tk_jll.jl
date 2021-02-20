cf94ac3e3fc36e0b1b724ebe4d31a836cf212973
jlbuild <juliabuildbot@gmail.com>
2021-02-20 19:43:39 +0000

    Tk_jll build 8.6.11+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -3,90 +3,21 @@ export libtk, wish
 
 using Tcl_jll
 using Xorg_libXft_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-
-# Relative path to `libtk`
-const libtk_splitpath = ["bin", "tk86.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libtk_path = ""
-
-# libtk-specific global declaration
-# This will be filled out by __init__()
-libtk_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libtk = "tk86.dll"
-
-
-# Relative path to `wish`
-const wish_splitpath = ["bin", "wish86.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-wish_path = ""
-
-# wish-specific global declaration
-function wish(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        if !isempty(get(ENV, LIBPATH_env, ""))
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', ENV[LIBPATH_env])
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(wish_path)
-    end
-end
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Tk")
+JLLWrappers.@declare_library_product(libtk, "tk86.dll")
+JLLWrappers.@declare_executable_product(wish)
 function __init__()
-    global artifact_dir = abspath(artifact"Tk")
+    JLLWrappers.@generate_init_header(Tcl_jll, Xorg_libXft_jll)
+    JLLWrappers.@init_library_product(
+        libtk,
+        "bin\\tk86.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (Tcl_jll.PATH_list, Xorg_libXft_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (Tcl_jll.LIBPATH_list, Xorg_libXft_jll.LIBPATH_list,))
+    JLLWrappers.@init_executable_product(
+        wish,
+        "bin\\wish86.exe",
+    )
 
-    global libtk_path = normpath(joinpath(artifact_dir, libtk_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libtk_handle = dlopen(libtk_path)
-    push!(LIBPATH_list, dirname(libtk_path))
-
-    global wish_path = normpath(joinpath(artifact_dir, wish_splitpath...))
-
-    push!(PATH_list, dirname(wish_path))
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(LIBPATH_list, ';')
-
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
